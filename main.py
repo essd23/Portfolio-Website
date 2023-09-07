8b0b4f83ec2eca22efdf2b7621f5849de144efb8
EssD <akinyemisaed@gmail.com>
2023-09-07 19:37:00 +0100

    Add descripton on the new column

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ col1, col2 = st.columns(2)
 with col2:
     st.title("Saheed Akinyemi")
     content = """
-    Hi, I'm Saheed! I am a Python Programmer, Network Engineer and a Cyber Security Enthusiast. I graduated in 2018 
-    and I currently work with NetIT solutions as a Network Engineer. Welcome to my portfolio website.
+  Hi, I'm Saheed! I am a Python Programmer, Network Engineer and a Cyber Security Enthusiast. I graduated in 2018
+with a Bachelors Degree from Osun State University. I currently work with NetIT solutions as a Network Engineer. 
+ Welcome to my portfolio website.
     """
     st.info(content)
+
+content2 = """
+    Below you can find some of the apps I hvave built in Python. Feel free to contact me."""
+
+st.write(content2)
